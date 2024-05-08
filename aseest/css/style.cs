4866a8008f1bc4a822d7978e0d22f214cd4eae15
jaykalathiya123 <kalathiyajay020>
2024-05-08 11:37:47 +0530

    sass pro

---
+++ aseest/css/style.cs
@@ -0,0 +1,221 @@
+header {
+  background-color: black;
+}
+
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --primary-100: #d5f1f1;
+  --primary: #23bcec;
+  --primary-rgb: 35, 188, 236;
+  --primary-900: #204f5e;
+  --primary-900-rgb: 32, 79, 94;
+  --secondary: #ecb934;
+  --secondary-: 236, 185, 52;
+  --black-color: #000000;
+  --black-rgb: 0, 0, 0;
+  --white: #ffffff;
+  --white-rgb: 255, 255, 255;
+  --bg: #faffff;
+  --bg-rgb: 250, 255, 255;
+  --text-color: #5a8695;
+  --font: Outfit;
+  --fs: 16px;
+  --fw: 300;
+  --lh: 1.5;
+  --gw: 15px;
+}
+
+.w-1 {
+  width: 8.33%;
+}
+
+.w-2 {
+  width: 16.66%;
+}
+
+.w-3 {
+  width: 25%;
+}
+
+.w-4 {
+  width: 33.33%;
+}
+
+.w-5 {
+  width: 41.66%;
+}
+
+.w-6 {
+  width: 50%;
+}
+
+.w-7 {
+  width: 58.33%;
+}
+
+.w-8 {
+  width: 66.66%;
+}
+
+.w-9 {
+  width: 75%;
+}
+
+.w-10 {
+  width: 83.33%;
+}
+
+.w-11 {
+  width: 91.66%;
+}
+
+.w-12 {
+  width: 100%;
+}
+
+.w-1,
+.w-2,
+.w-3,
+.w-4,
+.w-5,
+.w-6,
+.w-7,
+.w-8,
+.w-9,
+.w-10,
+.w-11,
+.w-12 {
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.w-n {
+  display: none;
+}
+
+.d-flex {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.col-gap {
+  column-gap: 5px;
+}
+
+ul,
+ol,
+li {
+  list-style: none;
+}
+
+body {
+  font-family: var(--jakarta);
+  overflow-x: hidden;
+  position: relative;
+}
+
+a {
+  text-decoration: none;
+}
+
+.justify-end {
+  justify-content: flex-end;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
+.col-gap-30 {
+  column-gap: 30px;
+}
+
+.align-center {
+  align-items: center;
+}
+
+.justify-start {
+  justify-content: center;
+}
+
+.justify-end {
+  justify-content: flex-end;
+}
+
+.justify-between {
+  justify-content: space-between;
+}
+
+.justify-around {
+  justify-content: space-around;
+}
+
+.justify-evenly {
+  justify-content: space-evenly;
+}
+
+.items-start {
+  align-items: flex-start;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.items-end {
+  align-items: flex-end;
+}
+
+.items-baseline {
+  align-items: baseline;
+}
+
+.items-stretch {
+  align-items: stretch;
+}
+
+.align-start {
+  align-content: flex-start;
+}
+
+.align-center {
+  align-content: center;
+}
+
+.align-end {
+  align-content: flex-end;
+}
+
+.align-between {
+  align-content: space-between;
+}
+
+.align-around {
+  align-content: space-around;
+}
+
+.align-evenly {
+  align-content: space-evenly;
+}
+
+.self-start {
+  align-self: flex-start;
+}
+
+.self-center {
+  align-self: center;
+}
+
+.self-end {
+  align-self: flex-end;
+}
+
+.align-start {
+  align-content: flex-start;
+}
+
+/*# sourceMappingURL=style.cs.map */
